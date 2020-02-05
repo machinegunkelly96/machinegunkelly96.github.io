9aa2cb091afcb42075623afd2061ffd8e0ff1421
Simone Ferrari <simone.ferrari1@outlook.com>
2020-02-05 17:45:55 +0100

    big commit

---
+++ fairytale.js
@@ -0,0 +1,13 @@
+function read() {
+	//	if (e == 1)
+
+	let para = document.createElement("p");
+	let node = document.createTextNode("This is a new paragraph.");
+	para.appendChild(node);
+	let element = document.getElementbyId("div1");
+	element.appendChild(para);
+
+	//else if (e == 2)
+	//	document.getElementbyId("frame").src = "mjolnir.html";
+
+}
