80b30f9ce630847bfef2318682a550bbd40177cc
Simone Ferrari <simone.ferrari1@outlook.com>
2020-02-05 19:08:55 +0100

    minor changes

--- fairytale.js
+++ fairytale.js
@@ -1,13 +1,24 @@
+let counter = 0;
+
 function read() {
-	//	if (e == 1)
 
-	let para = document.createElement("p");
-	let node = document.createTextNode("This is a new paragraph.");
-	para.appendChild(node);
-	console.log(para);
-	let element = document.getElementById("div1");
-	element.appendChild(para);
-
+	if (counter == 0) {
+		/*let para = document.createElement("p");
+		let node = document.createTextNode("This is a new paragraph");
+		para.appendChild(node);*/
+		let frame = document.createElement("iframe");
+		frame.setAttribute("src", "Mimir.html");
+		frame.style.border = "none";
+		frame.style.width = "inherit";
+		frame.style.height = "auto";
+		frame.style.marginTop = "50em";
+		let element = document.getElementById("main");
+		element.appendChild(frame);
+		counter++;
+	} else {
+		frame.remove();
+		counter = 0;
+	}
 	//else if (e == 2)
 	//	document.getElementbyId("frame").src = "mjolnir.html";
 
